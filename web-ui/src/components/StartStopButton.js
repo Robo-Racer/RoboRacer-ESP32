3bc1868c2f9fa425175e42c2f5c6cddfb0b2f1ac
antoniagaete <agaetep@hotmail.com>
2024-03-14 14:33:38 -0700

    Responsive to screen size

--- web-ui/src/components/StartStopButton.js
+++ web-ui/src/components/StartStopButton.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { useState } from 'react';
+import { Radio } from '@mui/material';
+import { styled } from '@mui/material';
+
+const CustomRadio = styled(Radio)(() => ({
+    '& .MuiSvgIcon-root': {
+        borderRadius: 0,
+        width: 100,
+        height: 100,
+    },
+    '& .MuiRadio-root': {
+        borderRadius: 0,
+    }
+}));
 
 function StartStopButton() {
 
     return (
         <div className='start-stop-container'>
-            
+            <CustomRadio />
+            <CustomRadio />
         </div>
     );
 }
