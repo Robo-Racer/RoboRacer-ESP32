89dc0dfee8da7ef60f66461b4a54c4a3032e59dc
antoniagaete <agaetep@hotmail.com>
2024-04-13 10:04:55 -0700

    Dev page linked with react router

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,33 +1,18 @@
 import React from 'react';
 import './App.css';
-import Controls from './components/Controls.js';
-import Performance from './components/Performance.js';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ControlsPage from './pages/ControlsPage';
+import DevPage from "./pages/DevPage";
 
 function App() {
+
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <div className="App">
-        <div className='header-container'>
-          <h1>RoboRacer</h1>
-        </div>
-
-        <div className='outer-container'>
-          <Controls />
-
-          {/* <Performance /> */}
-        </div>
-
-      </div>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ControlsPage />}></Route>
+        <Route path="/dev" element={<DevPage />}></Route>
+      </Routes>
+    </BrowserRouter>
     
   );
 }
