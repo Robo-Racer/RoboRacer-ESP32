5225ee7ed139a0ae29713158deff2295315e26f0
antoniagaete <agaetep@hotmail.com>
2024-04-13 10:27:12 -0700

    Added joystick

--- web-ui/src/components/DevControls.js
+++ web-ui/src/components/DevControls.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Joystick } from 'react-joystick-component';
 
 function DevControls() {
+    const handleMove = (e) => {
+        console.log(e);
+    }
+
     return (
-        <div className='controls-container'>
-            
+        <div className='dev-controls-container'>
+            <Joystick
+                stickColor='orange'
+                baseColor='black'
+                size={300}
+                throttle={200}
+                move={handleMove}
+                className="joystick" />
         </div>
     );
 }
