25dcc6522cf1b459267819342b9f10d4a0c3348c
Zak <zakarieleskowsky@gmail.com>
2024-06-03 20:15:20 -0700

    Fixing Portenta Communication Errors

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,27 @@
 #define FORMAT_SPIFFS_IF_FAILED true
 
 #define PORTENTA_BAUDRATE 9600
-#define UART_TX 43
-#define UART_RX 44
+// #define UART_TX 43
+// #define UART_RX 44
+#define UART_TX 17
+#define UART_RX 18
+
+//COMMUNICATION ENUMS
+enum messageHeader{
+    COMM_ERR = -1,
+    STOP = 1, 
+    START = 2,
+    READYTOSTART = 3,
+    DATA = 4,
+};
+
+//COMMUNICATION ENUMS
+enum dataHeader{
+    DATA_ERR = -1,
+    SPEED = 1,
+    DISTANCE = 2,
+};
+
 
 // Wifi name and password
 const char *ssid = "ESP32-Access-Point";
@@ -18,20 +37,198 @@ const char *password = "123456789";
 // Set web server port number to 80
 AsyncWebServer server(80);
 String receivedMessage;
-
+int starting;
 
 // Create a WebSocket object
 AsyncWebSocket ws("/ws");
 
-struct uartPacket {
-    String header;
-    String data;
-};
 
-//THINGS TO GET WORKING:
-//Send messages to user 
-//Communication protocol to H7
+messageHeader serial_get_message();
+void serial_send_message(messageHeader mHeader, dataHeader dHeader, String data);
+void notifyClients(String message);
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len);
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
+void initWebSocket();
+void listDir(fs::FS &fs, const char *dirname, uint8_t levels);
+void notFound(AsyncWebServerRequest *request);
+bool handlepostData(AsyncWebServerRequest *request, uint8_t *datas);
+void process_data();
 
+void setup()
+{
+    starting = 0;
+    // Baud rate for is 9600, communication with Serial Monitor
+    Serial.begin(9600);
+
+    delay(1000);
+
+    // Setup communication with Portenta H7
+    Serial1.begin(PORTENTA_BAUDRATE, SERIAL_8N1, UART_RX, UART_TX);
+
+    delay(1000);
+
+    // Set up file system via SPIFFS
+    if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED))
+    {
+        Serial.println("An Error has occurred while mounting SPIFFS");
+        return;
+    }
+
+    // Check our directories
+    // listDir(SPIFFS, "/", 0);
+
+    // Setup Access Point with SSID and password
+    Serial.print("Setting AP (Access Point)…");
+    WiFi.softAP(ssid, password, 1, 0, 1); //channel (default 1), ssid_hidden (default 0), max_connections set to 1
+
+    // Get IP address for connecting
+    //IP Address: 192.168.4.1
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+
+    server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+
+    server.onRequestBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
+      if (request->method() == HTTP_POST) {
+         if (!handlepostData(request, data)) {
+            Serial.print("Something went wrong!!!");
+            request->send(400, "text/plain", "false");
+         }
+         request->send(200, "text/plain", "true");
+      } 
+      if (request->method() == HTTP_GET) {
+        //Nothing yet
+      }
+      });
+    server.onNotFound(notFound);
+
+    initWebSocket();
+    server.begin();    // Start server, we can connect to it via device now
+
+    //Send READY to H7
+    serial_send_message(READYTOSTART, DATA_ERR, " ");
+    Serial.print("Sent Ready to start to Portenta!");
+    // delay(1000);
+
+    //Wait for READY from H7
+    while(1){
+        if(Serial1.available() > 0){
+            if(serial_get_message() == READYTOSTART){
+                break;
+            }
+        }
+    }
+    Serial.print("Got Ready to start to Portenta!");
+    starting = 1;
+}
+
+void loop() {
+    // ws.cleanupClients();
+    // if(ws.availableForWriteAll()){
+    //     notifyClients("Websocket Message sent?");
+    // }
+    // delay(1000);
+    // Serial.println("Inside Loop!");
+}
+
+//Send data
+//Wait half a second
+//If no confirmation recieved, send data again
+//Do this 3 times
+//If no confirmation, error out message to user
+//if confirmation recieved, good.
+
+messageHeader serial_get_message(){
+
+    String recievedMessage;
+    String headerStr = "ok";
+    messageHeader recievedHeader;
+
+    while(Serial1.available() > 0){
+      if(Serial1.available() > 0){
+        headerStr = Serial1.readStringUntil(',');
+      }
+      Serial.println(headerStr);
+      Serial.println(headerStr.toInt());
+
+      recievedHeader = (messageHeader)(headerStr.toInt());
+
+      Serial.println(recievedHeader);
+
+      switch(recievedHeader){
+        case COMM_ERR:
+          recievedMessage = Serial1.readStringUntil('\n'); //clear the buffer
+          notifyClients(receivedMessage);
+          break;
+
+        case STOP:
+          recievedMessage = Serial1.readStringUntil('\n'); //clear the buffer
+          break;
+
+        case START:
+          recievedMessage = Serial1.readStringUntil('\n'); //clear the buffer
+          break;
+
+        case READYTOSTART:
+          recievedMessage = Serial1.readStringUntil('\n'); //clear the buffer
+          notifyClients("The RoboRacer is ready to start!");
+          break;
+
+        case DATA:
+          process_data();
+          break;
+
+        default:
+          recievedMessage = Serial1.readStringUntil('\n'); //clear the buffer
+          break;
+      }
+
+    //   notifyClients(recievedMessage);
+
+    }
+    return recievedHeader;
+}
+
+void serial_send_message(messageHeader mHeader, dataHeader dHeader, String data){
+    Serial.print("Expecting: ");
+    Serial.print(mHeader);
+  switch(mHeader){
+    case COMM_ERR:
+      Serial1.print(mHeader);
+      Serial1.print(",");
+      Serial1.println(data);
+      break;
+
+    case START:
+      Serial1.print(mHeader);
+      Serial1.print(",");
+      break;
+
+    case STOP:
+      Serial1.print(mHeader);
+      Serial1.print(",");
+      break;
+
+    case READYTOSTART:
+      Serial1.print(mHeader);
+      Serial1.print(",");
+      break;
+
+    case DATA:
+      Serial1.print(mHeader);
+      Serial1.print(",");
+      Serial1.print(dHeader);
+      Serial1.print(",");
+      Serial1.println(data);
+      break;
+
+    default:
+      Serial1.print(mHeader);
+      Serial1.println(",");
+      break;
+  }
+}
 
 void notifyClients(String message) {
   ws.textAll(message);
@@ -40,13 +237,7 @@ void notifyClients(String message) {
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo*)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
-    //data[len] = 0;
-    //String message = (char*)data;
-    // Check if the message is "getReadings"
-    //if (strcmp((char*)data, "getReadings") == 0) {
-      //if it is, send current sensor readings
-      Serial.print((char*)data);
-    //}
+    //   Serial.print((char*)data);
   }
 }
 
@@ -126,6 +317,11 @@ void notFound(AsyncWebServerRequest *request)
 
 bool handlepostData(AsyncWebServerRequest *request, uint8_t *datas)
 {
+    if(starting == 0){
+        notifyClients("The RoboRacer isn't ready to process this yet!");
+        return true;
+    }
+
     Serial.printf("[REQUEST]\t%s\r\n", (const char *)datas);
 
     JsonDocument jsonDoc;
@@ -139,162 +335,56 @@ bool handlepostData(AsyncWebServerRequest *request, uint8_t *datas)
 
     if (jsonDoc.containsKey("name")){
         String _name = jsonDoc["name"].as<String>();
-        Serial1.println(_name);
+        // Serial1.println(_name);
     }
     if (jsonDoc.containsKey("time")){
         String _time = jsonDoc["time"].as<String>();
-        Serial1.println(_time);
+        // Serial1.println(_time);
     }
     if (jsonDoc.containsKey("distance")){
         String _distance = jsonDoc["distance"].as<String>();
-        Serial1.println(_distance);
+        serial_send_message(START, DATA_ERR, " ");
+        // Serial1.println(_distance);
     }
     if (jsonDoc.containsKey("direction")){
         String _direction = jsonDoc["direction"].as<String>();
-        Serial1.println(_direction);
+        // Serial1.println(_direction);
     }
     if (jsonDoc.containsKey("directive")){
         String _directive = jsonDoc["directive"].as<String>();
-        Serial1.println(_directive);
+        Serial.println(_directive);
+        serial_send_message(STOP, DATA_ERR, " ");
+        // Serial1.println(_directive);
+        // if(_directive == "STOP"){
+        //     serial_send_message(STOP, DATA_ERR, " ");
+        // } else if (_directive == "START") {
+        //     serial_send_message(START, DATA_ERR, " ");
+        // }
     }
 
-    Serial1.println("\n");
-    // if (!jsonDoc.containsKey("time"))
-    //     return false;
-    // String _time = jsonDoc["time"].as<String>();
-    // Serial.println(_time);
-
-    // if (!jsonDoc.containsKey("distance"))
-    //     return false;
-    // String _distance = jsonDoc["distance"].as<String>();
-    // Serial.println(_distance);
+    Serial.println("\n");
 
     return true;
 }
 
-void setup()
-{
-    // Baud rate for is 460800, communication with Serial Monitor
-    Serial.begin(460800);
+void process_data(){
+  String headerStr;
+  String recievedMessage;
+  dataHeader recievedHeader;
 
-    // Setup communication with Portenta H7
-    // Serial1.begin(PORTENTA_BAUDRATE, SERIAL_8N1, UART_RX, UART_TX);
+  headerStr = Serial1.readStringUntil(',');
+  recievedHeader = (dataHeader)(headerStr.toInt());
+  
+  switch(recievedHeader){
+    case SPEED:
+      recievedMessage = Serial1.readStringUntil('\n'); //clear the buffer
+      break;
 
-    delay(1000);
+    case DISTANCE:
+      recievedMessage = Serial1.readStringUntil('\n'); //clear the buffer
+      break;
 
-    // Serial1.println("y");
-    // uartPacket packet;
-    // while(1){
-    //     while (Serial1.available() > 0) {
-    //         char receivedChar = Serial1.read();
-    //         if (receivedChar == 'y') {
-    //             Serial.println(receivedMessage);  // Print the received message in the Serial monitor
-    //             break;
-    //         }
-    //     }
-    // }
-    
-
-    // Set up file system via SPIFFS
-    if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED))
-    {
-        Serial.println("An Error has occurred while mounting SPIFFS");
-        return;
-    }
-
-    // Check our directories
-    // listDir(SPIFFS, "/", 0);
-
-
-    // Setup Access Point with SSID and password
-    Serial.print("Setting AP (Access Point)…");
-    WiFi.softAP(ssid, password, 1, 0, 1); //channel (default 1), ssid_hidden (default 0), max_connections set to 1
-
-    // Get IP address for connecting
-    // NOTE: SHOULD LATER LOOK INTO HAVING PERMANENT SET IP ADDRESS
-    // NOTE FOR NOTE: THE IP ADDRESS DOESN'T SEEM TO CHANGE?
-    //IP Address: 192.168.4.1
-    IPAddress IP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(IP);
-
-    // Trying a serveStatic solution
-    // https://blockdev.io/react-on-the-esp32/
-    // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    //     request->send(SPIFFS, "/index.html", "text/html");
-    // });
-
-    server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
-    // server.serveStatic("/static/", SPIFFS, "/");
-
-    // server.on("/postData", HTTP_POST, [](AsyncWebServerRequest *request){
-    //    Serial.println("Received request for /postData");
-    //    Serial.printf("Type of content: %s", request->contentType());
-    //    int headers = request->headers();
-    //    int i;
-    //    for(i=0;i<headers;i++){
-    //       AsyncWebHeader* h = request->getHeader(i);
-    //       Serial.printf("HEADER[%s]: %s\n", h->name().c_str(), h->value().c_str());
-    //    }
-
-    //    int params = request->params();
-    //    Serial.printf("Number of parameters: %d", params);
-    //    for(int i = 0; i < params; i++){
-    //       AsyncWebParameter* p = request->getParam(i);
-    //       if(p->isFile()){ //p->isPost() is also true
-    //          Serial.printf("FILE[%s]: %s, size: %u\n", p->name().c_str(), p->value().c_str(), p->size());
-    //       } else if(p->isPost()){
-    //          Serial.printf("POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
-    //       } else {
-    //          Serial.printf("GET[%s]: %s\n", p->name().c_str(), p->value().c_str());
-    //       }
-    //    }
-
-    //    if (request->hasParam("value", true)) {
-    //       String postData;
-    //       AsyncWebParameter* bodyParam = request->getParam("value", true);
-    //       postData = bodyParam->value();
-    //       // Process postData as needed
-    //       Serial.println("Received POST data:");
-    //       Serial.println(postData);
-    //       request->send(200, "text/plain", "Data received successfully");
-    //    } else {
-    //       request->send(400, "text/plain", "Bad Request: 'value' parameter missing");
-    //    }});
-    server.onRequestBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
-      if (request->method() == HTTP_POST) {
-         if (!handlepostData(request, data)) {
-            Serial.print("Something went wrong!!!");
-            request->send(400, "text/plain", "false");
-         }
-         request->send(200, "text/plain", "true");
-      } 
-      if (request->method() == HTTP_GET) {
-        //Nothing yet
-      }
-      });
-
-    server.onNotFound(notFound);
-    
-
-    initWebSocket();
-
-    // Start server, we can connect to it via device now
-    server.begin();
+    default:
+      break;
+  }
 }
-
-void loop() {
-    // ws.cleanupClients();
-    if(ws.availableForWriteAll()){
-        notifyClients("Websocket Message sent?");
-    }
-    delay(1000);
-}
-
-
-//Send data
-//Wait half a second
-//If no confirmation recieved, send data again
-//Do this 3 times
-//If no confirmation, error out message to user
-//if confirmation recieved, good.
